98cefb6c45ffa01b8ad02e3ffb5454c179edbba7
nell.szymanski <nelliesimone@gmail.com>
2023-08-12 09:50:29 -0400

    second commit 8/12/23

---
+++ src/components/AnalogClock.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Clock from "./clocks";
+
+const AnalogClock = () => {
+  // analog clock implementation goes here
+  return <Clock type="analog" />;
+};
+
+export default AnalogClock;
---
+++ src/components/DigitalClock.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Clock from "./clocks";
+
+const DigitalClock = () => {
+  // Your analog clock implementation goes here
+  return <Clock type="digital" />;
+};
+
+export default DigitalClock;
